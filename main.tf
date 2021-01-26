627c7f8cfe07c2bcaabe68239285844a623c66c3
Marco Frömbgen <marco@justdice.io>
2021-01-26 16:35:27 +0100

    feat: add-module;

---
+++ main.tf
@@ -0,0 +1,45 @@
+module "label" {
+  source      = "applike/label/aws"
+  version     = "1.0.2"
+  project     = var.project
+  application = var.application
+  family      = var.family
+  environment = var.environment
+}
+
+locals {
+  cluster_name = "${module.label.project}-${module.label.environment}-${module.label.family}"
+  service_name = module.label.application
+}
+
+resource "aws_appautoscaling_target" "default" {
+  count              = var.enabled ? 1 : 0
+  service_namespace  = "ecs"
+  resource_id        = "service/${local.cluster_name}/${local.service_name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  max_capacity       = var.max_capacity
+  min_capacity       = var.min_capacity
+}
+
+resource "aws_appautoscaling_policy" "default" {
+  count              = var.enabled ? 1 : 0
+  name               = module.label.id
+  policy_type        = "TargetTrackingScaling"
+  service_namespace  = aws_appautoscaling_target.default[count.index].service_namespace
+  resource_id        = aws_appautoscaling_target.default[count.index].resource_id
+  scalable_dimension = aws_appautoscaling_target.default[count.index].scalable_dimension
+
+  dynamic "target_tracking_scaling_policy_configuration" {
+    for_each = var.target_tracking_configuration
+    content {
+      predefined_metric_specification {
+        predefined_metric_type = lookup(target_tracking_scaling_policy_configuration.value, "predefined_metric_type", "ECSServiceAverageCPUUtilization")
+        resource_label         = lookup(target_tracking_scaling_policy_configuration.value, "resource_label", null)
+      }
+
+      target_value       = lookup(target_tracking_scaling_policy_configuration.value, "target_value", 75)
+      scale_in_cooldown  = lookup(target_tracking_scaling_policy_configuration.value, "scale_in_cooldown", 60)
+      scale_out_cooldown = lookup(target_tracking_scaling_policy_configuration.value, "scale_out_cooldown", 60)
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,55 @@
+variable "enabled" {
+  type        = bool
+  description = "Set to false to prevent the module from creating any resources"
+  default     = true
+}
+
+variable "project" {
+  type        = string
+  default     = ""
+  description = "Project, which could be your organization name or abbreviation, e.g. 'eg' or 'cp'"
+}
+
+variable "environment" {
+  type        = string
+  default     = ""
+  description = "Environment, e.g. 'uw2', 'us-west-2', OR 'prod', 'staging', 'dev', 'UAT'"
+}
+
+variable "family" {
+  type        = string
+  default     = ""
+  description = "Family, e.g. 'prod', 'staging', 'dev', OR 'source', 'build', 'test', 'deploy', 'release'"
+}
+
+variable "application" {
+  type        = string
+  default     = ""
+  description = "Solution application, e.g. 'app' or 'jenkins'"
+}
+
+variable "max_capacity" {
+  type        = number
+  default     = ""
+  description = "The max capacity of the scalable target"
+}
+
+variable "min_capacity" {
+  type        = number
+  default     = ""
+  description = "The min capacity of the scalable target"
+}
+
+variable "target_tracking_configuration" {
+  type = list(object({
+    target_value       = number
+    scale_in_cooldown  = number
+    scale_out_cooldown = number
+    predefined_metric_specification = list(object({
+      predefined_metric_type = string
+      resource_label         = string
+    }))
+  }))
+  description = "A target tracking policy, requires policy_type = 'TargetTrackingScaling'"
+  default     = []
+}
